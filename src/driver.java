e79bcb6fbee29b1d765e5f4249b0fa4c0495c648
Matthew Ortiz <Mattheww.Ortiz@gmail.com>
2019-05-07 16:10:31 -0700

    Everything

--- src/driver.java
+++ src/driver.java
@@ -15,24 +15,30 @@ public class driver {
      */
     static boolean printRepos = false;
     static HashMap<String, page> inOut;
-    static LinkedList<page> ranks;
+    //static LinkedList<page> ranks;
     
     public static void main(String[] args) {
         inOut = new HashMap<>();
-        ranks = new LinkedList<>();
+//        ranks = new LinkedList<>();
         String url = "https://gameofthrones.fandom.com/wiki/Game_of_Thrones_(TV_series)";
-        crawl(url);
+        crawl(url, url);
         initRanks();
     }
     
-    private static void crawl(String url) {
+    private static void crawl(String url, String from) {
         try {
+//        	System.out.print('.');
+//        	System.out.println(inOut.size());
+        	if (inOut.size() == 50) {
+        		return;
+        	}
             if (!url.contains("https://gameofthrones.fandom.com/wiki/")) {
                 String temp = "https://gameofthrones.fandom.com".concat(url);
                 url = temp;
             }
             if (inOut.containsKey(url)) {
                 inOut.get(url).inlinks++;
+                inOut.get(url).linkedFrom.add(from);
                 return;
             } else {
                 inOut.put(url, new page(url));
@@ -40,7 +46,7 @@ public class driver {
             Connection con = Jsoup.connect(url);            
             Document doc = con.get();
 
-            System.out.println(url);
+//            System.out.println(url);
           
             if(printRepos) {
                 String fileName = url.substring(8).replace('/', '_').replaceAll("[?&]", "");
@@ -58,6 +64,7 @@ public class driver {
             
             String urlHref;
             for (int i = 0; i < links.size(); i++) {
+//            	System.out.println("Help me");
                 if (links.get(i) != null) {
                     urlHref = links.get(i).attr("href");
                     if(urlHref.contains("/wiki/") 
@@ -68,13 +75,15 @@ public class driver {
                         
                         inOut.get(url).outlinks++;
                         if (!inOut.get(url).linksTo.containsKey(urlHref)) {
+                        	//System.out.println(urlHref);
                             inOut.get(url).linksTo.put(urlHref, 1);
                         } else {
                             int l = inOut.get(url).linksTo.get(urlHref);
                             inOut.get(url).linksTo.replace(urlHref, l, l + 1);
                         }
                         
-                        crawl(urlHref);
+                        
+                        crawl(urlHref, url);
                     }
                 }
             }
@@ -90,28 +99,101 @@ public class driver {
     private static void initRanks() {
         int s = inOut.size();
         Iterator it = inOut.entrySet().iterator();
+        page[] ranks = new page[s];
+        double[] ranksOld = new double[s];
+        int ind = 0;
         while (it.hasNext()) {
             Map.Entry pair = (Map.Entry)it.next();
             page p = (page) pair.getValue();
             if (p.outlinks != 0) {
-                p.pageRank = 1 / s;
-                ranks.add(p);
+                p.pageRank = (double) 1 / s;
+                ranks[ind] = p;
             } else {
                 it.remove();
             }
+            ind++;
+//            System.out.println(p.name + " " + p.inlinks + " " + p.outlinks + " " + p.index);
+//            System.out.print("init pageRank ");
+//            System.out.println(1.0/s); // initial page rank
         }
+//        ranksOld = Arrays.copyOf(ranks, s);
+        for (int i = 0; i < s; i++) {
+        	ranksOld[i] = ranks[i].pageRank;
+        }
+        
+        boolean loop = true;
+        int iters = 0;
+
+        System.out.println();
+        while (loop) {
+//        	System.out.println("I mean it");
+//        	System.out.print('.');
+        	int ctr = 0;
+        	iters++;
+	        for (int i = 0; i < s-1; i++) {
+//	        	System.out.print(ranks[i].pageRank + " ");
+	        	ranks[i].pageRank = 0;
+	            for (int j = 0; j < s-1; j++) {
+	            	if (i != j && ranks[i].linkedFrom.contains(ranks[j].name)) {
+	            		ranks[i].pageRank += ranks[j].pageRank / ranks[j].outlinks;
+	            
+//	            		System.out.println(ranks[j].pageRank + " " + ranks[j].outlinks);
+	            	}
+	            	
+	            }
+//	            System.out.println(ranks[i].pageRank);
+	        }
+//	        System.out.println();
+	        for (int i = 0; i < s; i++) {
+	        	if (Math.abs(ranks[i].pageRank - ranksOld[i]) <= .001) {
+//	        		System.out.print(ranks[i].pageRank + " ");
+//	        		System.out.println(ranksOld[i]);
+	        		ctr++;
+	        		if (ctr == s) {
+	        			loop = false;
+	        			break;
+	        		}
+	        	}
+	        }
+	        for (int i = 0; i < s; i++) {
+	        	ranksOld[i] = ranks[i].pageRank;
+	        }
+	        if (iters == 10) {
+	        	loop = false;
+	        }
+        }
+        System.out.println(iters);
+        for (int i = 0; i< s-1; i ++) {
+        	System.out.println(ranks[i].pageRank + " " + ranks[i].name);
+        }
+
     }
 
     private static class page {
         String name;
-        int inlinks, outlinks;//number of inlinks and outlinks
-        float pageRank;//Current pageRank for a given Page
-        HashMap<String, Integer> linksTo;//What pages the current page links to and how many times
-
+        int inlinks, outlinks; //number of inlinks and outlinks
+        double pageRank; //Current pageRank for a given Page
+        HashMap<String, Integer> linksTo; //What pages the current page links to and how many times
+        LinkedList<String> linkedFrom;
+        void print(){
+    	   int o =0;
+        for (String n: linksTo.keySet()){
+            String key = n.toString();
+            String value = linksTo.get(n).toString();  
+            System.out.print(key + " " + value + " "); 
+            System.out.println((float)linksTo.get(n)/outlinks);
+    
+            if (o%linksTo.size()==0){
+            	//System.out.println();
+            }
+        }
+       	}
+        
         public page(String s) {
             name = s;
             inlinks = 1;
             linksTo = new HashMap<>();
+            linkedFrom = new LinkedList<>();
         }
     }
 }
