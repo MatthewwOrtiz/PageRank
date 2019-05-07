d3d251ffb7de6b1a7223cc47b26a5d5e45c9d752
mwono <michaelwono@gmail.com>
2019-05-06 21:27:04 -0700

    UpdatedCrawl to GoTWiki

--- src/driver.java
+++ src/driver.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
 import java.util.HashSet;
+import java.util.regex.Pattern;
 import org.jsoup.Connection;
 import org.jsoup.Connection.Response;
 
@@ -27,84 +28,51 @@ public class driver {
 	
 	
     public static void main(String[] args) {
-        String url = "https://old.reddit.com/r/MuseumOfReddit/";
+        String url = "https://gameofthrones.fandom.com/wiki/Game_of_Thrones_(TV_series)";
         HashSet<String> visited = new HashSet<>();
         crawl(url, visited);
     }
     
     private static void crawl(String url, HashSet<String> visited) {
         try {
-            while (url.contains("comments") && url.length() > 55) {
-                url = url.substring(0, url.lastIndexOf('/'));
-            }
-            if (visited.contains(url)) {
+            String page = url.replace("https://gameofthrones.fandom.com/wiki/", "");
+            if (visited.contains(page)) {
                 return;
             } else {
-                visited.add(url);
+                visited.add(page);
             }
-            
+            if (!url.contains("https://gameofthrones.fandom.com/wiki/")) {
+                String temp = "https://gameofthrones.fandom.com".concat(url);
+                url = temp;
+            }
             Connection con = Jsoup.connect(url);
-            
-            //delay the crawl
-            Thread.sleep(3000);
             
             Document doc = con.get();
             String fileName = url.substring(8).replace('/', '_').replaceAll("[?&]", "");
             if(fileName.length() < 2) {
-                    fileName = "Reddit";
+                    fileName = "GoTWiki";
             }
             
             System.out.println(url);
           
             if(printRepos) {
             new File("repository").mkdir();
-           PrintWriter pw = new PrintWriter("repository/" + fileName + ".html");
+                PrintWriter pw = new PrintWriter("repository/" + fileName + ".html");
 
-           pw.print(doc);
-           pw.close();
+                pw.print(doc);
+                pw.close();
             }
-            
-            Elements links = doc.select("[href*=https://old.reddit.com/r/MuseumOfReddit/comments/]");
-            Elements nextPage = doc.select("[href][rel=nofollow next]");
-            if (nextPage != null) {
-                links.add(nextPage.first());
-            }
-            
-            
-            Elements comments = doc.select("a[href]");
-           
-            StringBuilder sb = new StringBuilder();
-            
-            for (int i = 0; i < comments.size(); i++) {
-                String s = comments.get(i).attr("href");
-                if (s.length() > 0 && s.contains("/user/")) {
-                	s = s.replace("https://old.reddit.com","   ");
-                	System.out.println(s);
-                	//sb.append(s + ',');
-                }
-                if (s.length() > 0 && s.contains("/u/")) {
-                	System.out.println(s);
-                	//sb.append(s + ',');
-                }
-                
-            }
-            
-            if(printComments) 
-            {
-                new File("comments").mkdir();
-	            if (sb.length() > 0) {
-	              PrintWriter pw2 = new PrintWriter("comments/" + fileName + ".html");
-	               pw2.print(sb.toString());
-	                pw2.close();
-	            }
-            }
-            
+
+            Elements links = doc.select("div#WikiaArticle").first().select("[href]");
             
             String urlHref;
             for (int i = 0; i < links.size(); i++) {
                 if (links.get(i) != null) {
-                    urlHref = links.get(i).attr("href").toLowerCase().replace("www", "old");
-                    if(urlHref.contains("http") && urlHref.contains("museumofreddit") && !urlHref.contains(".rss")) {
+                    urlHref = links.get(i).attr("href");
+                    if(urlHref.contains("/wiki/") 
+                            && !urlHref.contains("?") 
+                            && !urlHref.substring(6).contains(":")
+                            && !urlHref.contains("wikipedia")) {
                         crawl(urlHref, visited);
                     }
                 }
@@ -113,8 +81,6 @@ public class driver {
             System.out.println("Bad URL: " + e.getMessage());
         } catch (IOException e) {
             System.out.println("Unable to Create Connection " + e.getMessage());
-        } catch (InterruptedException e) {
-            System.out.println(e.getMessage());
         }
     }
     
